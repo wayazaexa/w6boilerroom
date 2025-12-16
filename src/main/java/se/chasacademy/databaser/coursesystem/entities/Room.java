4fc692fd47468d71dad3f65d01010a1d96f6344a
Joakim Englund <joakimenglund@protonmail.com>
2025-12-16 09:15:13 +0100

    Created Room

---
+++ src/main/java/se/chasacademy/databaser/coursesystem/entities/Room.java
@@ -0,0 +1,60 @@
+package se.chasacademy.databaser.coursesystem.entities;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Min(1)
+    private int capacity;
+
+    @OneToMany(mappedBy = "room")
+    List<CourseSession> sessions;
+
+    public Room() {}
+
+    public Room(String name, String address, int capacity) {
+        this.name = name;
+        this.address = address;
+        this.capacity = capacity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}

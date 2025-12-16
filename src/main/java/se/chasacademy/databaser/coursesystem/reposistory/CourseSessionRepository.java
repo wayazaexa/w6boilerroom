7afe971fb1d4744c2c7ef4634e82aa6e2fa3887b
Joakim Englund <joakimenglund@protonmail.com>
2025-12-16 14:08:24 +0100

    Fixed annotations

--- src/main/java/se/chasacademy/databaser/coursesystem/reposistory/CourseSessionRepository.java
+++ src/main/java/se/chasacademy/databaser/coursesystem/reposistory/CourseSessionRepository.java
@@ -1,11 +1,13 @@
 package se.chasacademy.databaser.coursesystem.reposistory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import se.chasacademy.databaser.coursesystem.entities.CourseSession;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface CourseSessionRepository extends JpaRepository<CourseSession, Long> {
 
     // all commend courses after days date
--- src/main/java/se/chasacademy/databaser/coursesystem/reposistory/ParticipantRepository.java
+++ src/main/java/se/chasacademy/databaser/coursesystem/reposistory/ParticipantRepository.java
@@ -2,10 +2,12 @@ package se.chasacademy.databaser.coursesystem.reposistory;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import se.chasacademy.databaser.coursesystem.entities.Participant;
 
 import java.util.Optional;
 
+@Repository
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
     Optional<Participant> findByEmail(String email);
 }
